5d234bb9ad4bcef36b007d0277faf0f329e29855
Igor Kandyba <igor.kandyba@gmail.com>
2012-08-05 22:45:43 +0300

    Making use of helpers to replace htmlspecialchars()

--- jokes-app/jokes.html.php
+++ jokes-app/jokes.html.php
@@ -1,3 +1,6 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'] .
+    '/includes/helpers.inc.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -15,12 +18,12 @@
       <form action="?deletejoke" method="post">
         <blockquote>
           <p>
-            <?php echo htmlspecialchars($joke['text'], ENT_QUOTES, 'UTF-8'); ?>
+            <?php htmlout($joke['text']) ?>
             <input type="hidden" name="id" value="<?php echo $joke['id']; ?>">
             <input type="submit" value="Delete">
             (by <a href="mailto:
-            <?php echo htmlspecialchars($joke['email'], ENT_QUOTES, 'UTF-8'); ?>">
-            <?php echo htmlspecialchars($joke['name'], ENT_QUOTES, 'UTF-8'); ?></a>)
+            <?php htmlout($joke['email']); ?>">
+            <?php htmlout($joke['name']) ?></a>)
           </p>
         </blockquote>
       </form>
